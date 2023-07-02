9d255b5e2f8a6d2bb3f4d3ce6ca494df4cad4036
master <79881790355@ya.ru>
2023-07-02 19:14:14 +0300

    feat: Add serializer methods

---
+++ InfrastructureService/Utils/CustomSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace InfrastructureService.Utils;
+
+public class CustomSerializer
+{
+    private static JsonSerializerOptions Options => 
+        new JsonSerializerOptions()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+    public static string Serialize<T>(T obj) where T : class
+    {
+        return JsonSerializer.Serialize(obj, Options);
+    }
+
+    public static T? Deserialize<T>(string json) where T : class
+    {
+        return JsonSerializer.Deserialize<T>(json);
+    }
+}
